6757116e98c13e86f4e3bc53feab902fa3c4ad6b
Thanh Tùng Phan <Thanhtung1482013@gmail.com>
2021-02-20 15:20:02 +0700

    Add new TraineeInfo Class , Layout , Style

---
+++ Models/TraineeInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrainingManagementSystem.Models
+{
+    public class TraineeInfo
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Name should not be empty")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Age should not be empty")]
+        public int Age { get; set; }
+
+
+        [Required(ErrorMessage = "ID Student should not be empty")]
+        public string IdStudent { get; set; }
+
+
+        [Required(ErrorMessage = "Trainee Account should not be empty")]
+        public string TraineeAccount { get; set; }
+        public string Password { get; set; }
+
+
+        public decimal TOEICScore { get; set; }
+
+
+        [Required(ErrorMessage = "Location should not be empty")]
+        public string Location { get; set; }
+
+        public string Description { get; set; }
+
+
+        [Required(ErrorMessage = "Date of Birth should not be empty")]
+        public DateTime DateBorn { get; set; }
+    }
+}
